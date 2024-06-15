b621d9532f97f8fe56944158d4880f42b2e7b571
sansahil <145114926+sansahil@users.noreply.github.com>
2024-06-15 21:44:14 +0530

    Add files via upload

---
+++ find the postive and negetive num.py
@@ -0,0 +1,10 @@
+
+# find postive or negetive number
+num =int(input("enter the number here: "))
+
+if num > 0:
+    print(" it is a postive number ")
+elif num == 0:
+    print("it is zero")
+else:
+    print("it is a negetive number")        
