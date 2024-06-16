6e77329ef157171d4ed0fbbb56e3703e7b8b968a
sansahil <145114926+sansahil@users.noreply.github.com>
2024-06-16 16:25:10 +0530

    py.05

---
+++ calculator.py
@@ -0,0 +1,14 @@
+num1 = int(input("enter the first number "))
+num2 = int(input("enter the second number "))
+
+op = input("enter the operation here ")
+
+if op == "+":
+    print(num1 + num2)
+if op == "-":
+    print(num1 - num2)
+if op == "*":
+    print(num1*num2)
+else:
+    print(num1/num2)      
+
